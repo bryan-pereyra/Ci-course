6951de31c97042c2bd79a7cf74bf57a13552e959
Bryan Pereyra <bryan.pereyra@unosquare.com>
2023-01-19 12:21:22 -0600

    First commit

---
+++ src/test/java/Training/FirstSeleniumJavaTest.java
@@ -0,0 +1,32 @@
+package Training;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumJavaTest {
+
+	private WebDriver driver;
+
+	@Test
+	public void testEasy() {
+		driver.get("https://testautomationu.applitools.com/");
+		driver.manage().window().maximize();
+
+		String title = driver.getTitle();
+		AssertJUnit.assertTrue(title.equals("Test Automation University | Applitools"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new ChromeDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
